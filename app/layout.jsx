2d446965eabbe6b10cd1258e1e3863acbbc4047a
Martin Costa Daddato <martin.costa.daddato@gmail.com>
2023-12-07 23:13:03 -0300

    pb added

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black`}>
+      <body className={`${inter.className} bg-black pb-12`}>
         <Navbar />
         {children}
         <SpeedInsights />
